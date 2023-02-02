6540d9659d39267263a0c2d128f837590f0b95d9
Bob Smith <keam@travelers.com>
2023-02-02 13:56:30 -0500

    doesn't matter

--- src/Fetch.js
+++ src/Fetch.js
@@ -11,22 +11,31 @@ function fetchPerson(id) {
 
 function fetchAllPeople() {
 
-    let people;
+    let people = [];
 
     let url = `https://swapi.dev/api/people`;
 
     while (url) {
         try {
-            const fetchPeople = fetch(url).then(res => res.json());
-            .then()
-            .then(res => {
-                url = res.next; 
-                return res
-            })
+            const fetchPeople = fetch(url).then(res => res.json())
+                .then(res => {
+                    url = res.next;
+                    return res
+                })
+                .then(res => res.results)
+                .then(res => res.map(p => ({ ...p, id: +getIdFrom(p.url) })))
+            people.push(...fetchPeople)
         } catch (ex) {
             console.error(`Error reading person ${id} data.`, ex.message);
         }
-        
+
     }
-    
+    return people;
+}
+
+const getIdFrom = (entityName, url) => {
+    const re = new RegExp(`.*${entityName}\/(\\d+).*`);
+    const matches = url.match(re)
+    if (!matches) throw `Bad URL. Not a ${entityName} URL.`
+    return matches[1]
 }
