daf4e124427b69538565e59511e2433638a6f7f4
Bob Smith <keam@travelers.com>
2023-02-02 12:07:37 -0500

    Made fetchPerson and fetchAllPeople functions.

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 
 export const Homepage = () => {
+
+    let people;
+
+
+    useEffect(() => {
+      
+    }, [])
+    
   return (
     <div>Homepage</div>
   )
