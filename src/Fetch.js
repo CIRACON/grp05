54c0a18e8e5aee80bc696422d000e7c90a1af778
Bob Smith <wasadmin@weagesolutions.com>
2023-02-02 14:34:26 -0500

    progress

--- src/Fetch.js
+++ src/Fetch.js
@@ -1,39 +1,57 @@
-function fetchPerson(id) {
+export async function fetchPerson(id) {
     let url = `https://swapi.dev/api/people/${id}`;
 
     try {
-        const person = fetch(url).then(res => res.json());
-        return person;
+        return await fetch(url).then(res => res.json());
     } catch (ex) {
         console.error(`Error reading person ${id} data.`, ex.message);
     }
 }
 
-function fetchAllPeople() {
+export async function fetchAllPeople() {
 
     let people = [];
 
-    let url = `https://swapi.dev/api/people`;
+    let url = `https://swapi.dev/api/people/`;
 
     while (url) {
         try {
-            const fetchPeople = fetch(url).then(res => res.json())
-                .then(res => {
-                    url = res.next;
-                    return res
-                })
+            const fetchPeople = await fetch(url)
+                .then(res => res.json())
+                .then(res => { url = res.next; return res })
                 .then(res => res.results)
-                .then(res => res.map(p => ({ ...p, id: +getIdFrom(p.url) })))
+                .then(res => res.map(p => ({ ...p, id: + getIdFrom('people', p.url) })))
+
             people.push(...fetchPeople)
         } catch (ex) {
-            console.error(`Error reading person ${id} data.`, ex.message);
+            console.error(`Error reading people.`, ex.message);
         }
+   }
 
-    }
     return people;
 }
 
-const getIdFrom = (entityName, url) => {
+export async function fetchFilm(id) {
+    let url = `https://swapi.dev/api/films/${id}`;
+
+    try {
+        return await fetch(url).then(res => res.json());
+    } catch (ex) {
+        console.error(`Error reading film ${id} data.`, ex.message);
+    }
+}
+
+export async function fetchPlanet(id) {
+    let url = `https://swapi.dev/api/planets/${id}`;
+
+    try {
+        return await fetch(url).then(res => res.json());
+    } catch (ex) {
+        console.error(`Error reading planet ${id} data.`, ex.message);
+    }
+}
+
+export function getIdFrom(entityName, url) {
     const re = new RegExp(`.*${entityName}\/(\\d+).*`);
     const matches = url.match(re)
     if (!matches) throw `Bad URL. Not a ${entityName} URL.`
--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,45 +1,16 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-
+import { fetchAllPeople } from '../../Fetch'
 
 export const Homepage = () => {
 
-    let people;
-
-    let getIdFrom;
-    let id;
-
-    function fetchAllPeople() {
-
-        let people = [];
-    
-        let url = `https://swapi.dev/api/people`;
-    
-        //while (url) {
-            try {
-                const fetchPeople = fetch(url).then(res => res.json())
-                    .then(res => {
-                        url = res.next;
-                        return res
-                    })
-                people.push(...fetchPeople)
-            } catch (ex) {
-                console.error(`Error reading people.`, ex.message);
-            }
-    
-       // }
-        console.log(people)
-        return people;
-    }
-
-    useEffect(() => {
-      console.log(fetchAllPeople)
-    }, [])
+  useEffect(() => {
+    fetchAllPeople().then(res => console.log(res))
+  }, [])
     
   return (
     <>
     <div>Homepage</div>
-    <button onClick={fetchAllPeople}>show me the people array</button>
     </>
   )
 }
