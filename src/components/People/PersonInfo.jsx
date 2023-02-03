c1793620882c4f99f8f2489bed578c0c4cb902af
Bob Smith <wasadmin@weagesolutions.com>
2023-02-03 11:11:59 -0500

    Working people/person pages

--- src/components/People/PersonInfo.jsx
+++ src/components/People/PersonInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { fetchFilm, fetchPerson, fetchPlanet, getIdFrom } from '../../Fetch'
+import '../components.css'
 
 export const PersonInfo = () => {
   const [person, setPerson] = useState({})
@@ -12,40 +13,54 @@ export const PersonInfo = () => {
   useEffect(() => {
     fetchPerson(id)
     .then(res => {setPerson(res)})
-    .then(() => {
-        console.log(person)
-        
-        fetchPlanet(getIdFrom('planets', person.homeworld))
-          .then(res => setPlanet(res))
-
-        person.films.forEach(film => {
-          fetchFilm(getIdFrom('films', film).then(res => setFilms(films.push(res))))
-        })
-      }
-    )
-
   }, [])
+
+  useEffect(() => {
+    if (Object.keys(person).length == 0)
+      return 
+        
+    fetchPlanet(getIdFrom('planets', person.homeworld))
+      .then(res => setPlanet(res))
+
+    console.log(planet)
+    person.films.forEach(film => {
+      fetchFilm(getIdFrom('films', film)).then(res => {
+        let check = 0;
+        films.forEach(film => {
+          if (film.title === res.title)
+            check = 1
+        })
+        
+        if (!check)
+          setFilms(oldFilmList => [...oldFilmList, res])
+      })
+    })
+
+    console.log(films)
+  }, [person])
 
   return (
     <>
       <div>
-        <h1>{person.name}</h1>
+        <h1 id="name">{person.name}</h1>
       </div>
-      <div>
+      <section id="generalInfo">
         <p>Height: {person.height} cm</p>
         <p>Mass: {person.mass} kg</p>
         <p>Born: {person.birth_year}</p>
-      </div>
-      <div>
+      </section>
+      <section id="planets">
         <h2>Homeworld</h2>
-        <p>{planet.name}</p>
-      </div>
-      <div>
+        <ul>
+          <li><a href={`/Planets/${planet.id}`}>{planet.name}</a></li>
+        </ul>
+      </section>
+      <section id="films">
         <h2>Films</h2>
         <ul>
-          {films.map(film => <li>{film.title}</li>)}
+          {films.map(film => <li><a href={`/Films/${film.id}`}>{film.title}</a></li>)}
         </ul>
-      </div>
+      </section>
     </>
   )
 }
