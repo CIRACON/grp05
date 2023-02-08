776b3b634ca892f3b07d139a36f71bae88ae93d2
Bob Smith <wasadmin@weagesolutions.com>
2023-02-08 15:44:24 -0500

    Add in employee filtering capabilities

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,22 @@ var app = express();
 app.use(cors());
 app.use(express.json());
 
+app.post("/filterEmployees", (req, res) => {
+    const validKeys = ['name', 'phone_number', 'job_role', 'work_location', 'division', 'department']
+    let body = req.body
+
+    Object.keys(body).forEach(
+        (key) => (validKeys.includes(key) && typeof body[key] === "string") || delete body[key]
+    );
+
+    dao.filterEmployees(body, (data) => {res.send(data)})
+})
+
 app.post("/login", (req, res) => {
     let credentials = req.body
     dao.login(credentials.username, credentials.password, (data) => {res.send(data)})
 })
 
-// server start-up
 const port = 4000
 console.log("Open a browser to http://localhost:"+port+" to view the application");
 app.listen(port);
